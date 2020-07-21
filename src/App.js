6608d1edf7be5a7c03ed1184fbbfc9bc496ce1bd
SaidBelahrach <said.belahrach16@gmail.com>
2020-07-21 20:15:20 +0000

    3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'; 
-import './App.css';         
+import './App.css';  
+import Form from './components/demandeur/Form';       
 
 
 function App(props) { 
     
     return <div>  
-                hello
+                <Form/>
            </div>
 } 
 export default App; 
